970ebedcde4593fdf7d0cca9bc6c95d793b885cf
SashaPavlovski <sasha123.ez@gmailcom>
2022-11-20 21:50:48 +0200

    home work

---
+++ BankLeomi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace files_2_CS
+{
+    internal class BankLeomi : BankEx3
+    {
+        public BankLeomi(string bankCode, string bankName, string branchCode, string branchAddress, string city, string telephone) : base(bankCode, bankName, branchCode, branchAddress, city, telephone) { }
+
+        private string _giftForNewCustomers;
+        public string GiftForNewCustomers
+        {
+            get { return _giftForNewCustomers; }
+            set { _giftForNewCustomers = value; }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace files_2_CS
+{
+    internal class Program
+    {
+       
+            static void Main(string[] args)
+            {
+                RunEx1 runEx1 = new RunEx1();
+                //runEx1.Run();
+
+                RunEx2 runEx2 = new RunEx2();
+                //runEx2.Run();
+
+                RunBankEx3 runBankEx3 = new RunBankEx3();
+                //runBankEx3.Run();
+
+                RunBankEx3 runBankEx31 = new RunBankEx3();
+                runBankEx31.Run2();
+
+                Console.ReadLine();
+            }
+        
+    }
+}
